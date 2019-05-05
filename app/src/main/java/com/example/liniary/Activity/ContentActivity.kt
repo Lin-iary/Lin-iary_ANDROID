c3842eec1fe6509bb00b2e982f5c62f1765a4dab
ganghee <idiosauri@naver.com>
2019-05-05 20:19:52 +0900

    aa

--- app/src/main/java/com/example/liniary/Activity/ContentActivity.kt
+++ app/src/main/java/com/example/liniary/Activity/ContentActivity.kt
@@ -1,13 +1,13 @@
 package com.example.liniary.Activity
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.example.liniary.R
 
-class MainActivity : AppCompatActivity() {
+class ContentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_content)
     }
 }
