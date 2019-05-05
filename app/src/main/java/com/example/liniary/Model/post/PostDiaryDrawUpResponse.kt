149049ab9786f6c0a88720139536935cd2aa1080
Leesohee <shfksthgml@naver.com>
2019-05-06 05:36:08 +0900

    일기 작성 통신 완료

--- app/src/main/java/com/example/liniary/Model/post/PostDiaryDrawUpResponse.kt
+++ app/src/main/java/com/example/liniary/Model/post/PostDiaryDrawUpResponse.kt
@@ -3,7 +3,6 @@ package com.example.liniary.Model.post
 data class PostDiaryDrawUpResponse (
     val status: Int,
     val success: Boolean,
-    val message : String,
-    val data : ArrayList<String>
+    val message : String
 // 필요없는 데이터(null)은 굳이 안써도 됨
 )
