c38a22c4dc9a6c57b41e828c892346fbf9f20fdb
ganghee <idiosauri@naver.com>
2019-05-06 05:29:24 +0900

    aaa

--- app/src/main/java/com/example/liniary/Activity/ContentActivity.kt
+++ app/src/main/java/com/example/liniary/Activity/ContentActivity.kt
@@ -11,6 +11,8 @@ import kotlinx.android.synthetic.main.activity_content.*
 import kotlinx.android.synthetic.main.navigation_main.view.*
 
 class ContentActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
+
+
     override fun onTabReselected(p0: TabLayout.Tab?) {
 
     }
@@ -22,29 +24,31 @@ class ContentActivity : AppCompatActivity(), TabLayout.OnTabSelectedListener {
     override fun onTabSelected(p0: TabLayout.Tab?) {
         viewpager_main.currentItem = p0!!.position
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
         configureMainTab()
     }
-    private fun configureMainTab() {
-        viewpager_main.adapter = MainPagerAdapter(supportFragmentManager, 3)
-        viewpager_main.offscreenPageLimit = 2
-        tl_main_category.setupWithViewPager(viewpager_main)
-
-        val navCategoryMainLayout: View =
-            (this.getSystemService(android.content.Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
-                .inflate(R.layout.navigation_main, null, false)
-        tl_main_category.getTabAt(0)!!.customView =
-            navCategoryMainLayout.navigation_category_diary
-        tl_main_category.getTabAt(1)!!.customView =
-            navCategoryMainLayout.navigation_category_drawup
-        tl_main_category.getTabAt(2)!!.customView =
-            navCategoryMainLayout.navigation_category_page
 
 
+private fun configureMainTab() {
+    viewpager_main.adapter = MainPagerAdapter(supportFragmentManager, 3)
+    viewpager_main.offscreenPageLimit = 2
+    tl_main_category.setupWithViewPager(viewpager_main)
 
-    }
+    val navCategoryMainLayout: View =
+        (this.getSystemService(android.content.Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+            .inflate(R.layout.navigation_main, null, false)
+    tl_main_category.getTabAt(0)!!.customView =
+        navCategoryMainLayout.navigation_category_diary
+    tl_main_category.getTabAt(1)!!.customView =
+        navCategoryMainLayout.navigation_category_drawup
+    tl_main_category.getTabAt(2)!!.customView =
+        navCategoryMainLayout.navigation_category_page
+
+
+}
 
 }
 
