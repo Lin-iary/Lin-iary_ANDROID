f38804365089b0ea9171757809e7881b5049d0e5
Leesohee <shfksthgml@naver.com>
2019-05-06 01:51:10 +0900

    a

---
+++ app/src/main/java/com/example/liniary/Model/PostDiaryDrawUpResponse.kt
@@ -0,0 +1,9 @@
+package com.example.liniary.Model
+
+data class PostDiaryDrawUpResponse (
+    val status: Int,
+    val success: Boolean,
+    val message : String,
+
+// 필요없는 데이터(null)은 굳이 안써도 됨
+)
--- app/src/main/java/com/example/liniary/Network/NetworkService.kt
+++ app/src/main/java/com/example/liniary/Network/NetworkService.kt
@@ -1,6 +1,7 @@
 package com.example.liniary.Network
 
 import android.support.annotation.NonNull
+import com.example.liniary.Model.PostDiaryDrawUpResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
@@ -8,4 +9,13 @@ import retrofit2.http.*
 
 interface NetworkService {
 
+    @Multipart
+    @POST("/api/upload")
+    fun postJoin(
+        @Part("user_id") user_id: RequestBody,
+        @Part("user_password") user_password: RequestBody,
+        @Part("user_name") user_name: RequestBody,
+        @Part("user_phone") user_phone: RequestBody,
+        @Part user_img: MultipartBody.Part?
+    ): Call<PostDiaryDrawUpResponse>
 }
