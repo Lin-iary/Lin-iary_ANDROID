f1474fa50bb40cb62b8686bc94d09eee6e5f0196
ganghee <idiosauri@naver.com>
2019-05-05 21:21:27 +0900

    viewPager init

--- app/src/main/java/com/example/liniary/Activity/ContentActivity.kt
+++ app/src/main/java/com/example/liniary/Activity/ContentActivity.kt
@@ -2,13 +2,38 @@ package com.example.liniary.Activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import com.example.liniary.Adapter.MainPagerAdapter
 import com.example.liniary.R
+import kotlinx.android.synthetic.main.activity_content.*
+import kotlinx.android.synthetic.main.navigation_main.view.*
 
 class ContentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
+
+        configureMainTab()
+    }
+    private fun configureMainTab() {
+        viewpager_main.adapter = MainPagerAdapter(supportFragmentManager, 4)
+        viewpager_main.offscreenPageLimit = 2
+        tl_main_category.setupWithViewPager(viewpager_main)
+
+        val navCategoryMainLayout: View =
+            (this.getSystemService(android.content.Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+                .inflate(R.layout.navigation_main, null, false)
+        tl_main_category.getTabAt(0)!!.customView =
+            navCategoryMainLayout.navigation_category_diary
+        tl_main_category.getTabAt(1)!!.customView =
+            navCategoryMainLayout.navigation_category_drawup
+        tl_main_category.getTabAt(2)!!.customView =
+            navCategoryMainLayout.navigation_category_consulting
+        tl_main_category.getTabAt(3)!!.customView =
+            navCategoryMainLayout.navigation_category_page
+
     }
 }
 
